29442c0e56e3319912f6b02945af2d1e5dcf20e0
Sougata <sougatadas9874@gmail.com>
2023-10-10 19:18:17 +0530

    add local search bar component.

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import RenderTags from "./RenderTags";
-import { Tangent } from "lucide-react";
 
 const RightSidebar = () => {
   const questions = [
---
+++ components/shared/search/LocalSearch.tsx
@@ -0,0 +1,57 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import { Input } from "@/components/ui/input";
+
+interface CustomInputProps {
+  route: string;
+  iconPosition: string;
+  imgSrc: string;
+  placeholder: string;
+  otherClasses: string;
+}
+
+const LocalSearch = ({
+  route,
+  iconPosition,
+  imgSrc,
+  placeholder,
+  otherClasses,
+}: CustomInputProps) => {
+  return (
+    <div
+      className={`background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4 ${otherClasses} `}
+    >
+      {iconPosition === "left" && (
+        <Image
+          src={imgSrc}
+          alt="search icon"
+          width={24}
+          height={24}
+          className="cursor-pointer"
+        />
+      )}
+
+      <Input
+        type="text"
+        value=""
+        onChange={() => {}}
+        placeholder={placeholder}
+        className="paragraph-regular no-focus 
+          background-light800_darkgradient  placeholder border-none shadow-none outline-none"
+      />
+
+      {iconPosition === "right" && (
+        <Image
+          src={imgSrc}
+          alt="search icon"
+          width={24}
+          height={24}
+          className="cursor-pointer"
+        />
+      )}
+    </div>
+  );
+};
+
+export default LocalSearch;
