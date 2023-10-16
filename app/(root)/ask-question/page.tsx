d57f2a6beb5fccbb24eb9b33e497a8c71ab55158
Sougata <sougatadas9874@gmail.com>
2023-10-16 18:45:08 +0530

    add form fields and questions schema.

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,9 +1,14 @@
-const page = () => {
+import Question from "@/components/forms/Question";
+
+const Page = () => {
   return (
     <div>
-      <h1>Ask-Question</h1>
+      <h1 className="h1-bold text-dark100_light900">Ask a Question </h1>
+      <div className="mt-9">
+        <Question />
+      </div>
     </div>
   );
 };
 
-export default page;
+export default Page;
