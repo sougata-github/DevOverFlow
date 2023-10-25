12a2cceb1583dd1cdc3e92c2c9bcf0ce9990e661
Sougata <sougatadas9874@gmail.com>
2023-10-25 18:46:50 +0530

    add createQuestion function and user server action.

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,11 +1,26 @@
 import Question from "@/components/forms/Question";
+import { getUserById } from "@/lib/actions/user.actions";
+// import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const Page = () => {
+const Page = async () => {
+  // const { userId } = auth();
+
+  const userId = "12345";
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const mongoUser = await getUserById({ userId });
+
+  console.log(mongoUser);
+
   return (
     <div>
       <h1 className="h1-bold text-dark100_light900">Ask a Question </h1>
       <div className="mt-9">
-        <Question />
+        <Question mongoUserId={JSON.stringify(mongoUser.id)} />
       </div>
     </div>
   );
