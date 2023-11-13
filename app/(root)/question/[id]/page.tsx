673f3731fe80199de7dbbbef218ec2a6bb6c3a6b
Sougata <sougatadas9874@gmail.com>
2023-11-13 20:34:26 +0530

    add voting ui to question details page.

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -42,7 +42,16 @@ const Page = async ({ params }: any) => {
             </p>
           </Link>
           <div className="flex justify-end">
-            <Votes />
+            <Votes
+              type="question"
+              itemId={JSON.stringify(result._id)}
+              userId={JSON.stringify(mongoUser._id)}
+              upvotes={result.upvotes.length}
+              hasUpvoted={result.upvotes.includes(mongoUser._id)}
+              downvotes={result.downvotes.length}
+              hasDownvoted={result.downvotes.includes(mongoUser._id)}
+              hasSaved={mongoUser?.saved.includes(result._id)}
+            />
           </div>
         </div>
         <h2 className="h2-semibold text-dark200_light900 mt-3.5 w-full text-left">
--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -1,5 +1,99 @@
-const Votes = () => {
-  return <div>Votes</div>;
+"use client";
+
+import { formatNumber } from "@/lib/utils";
+import Image from "next/image";
+
+interface Props {
+  type: string;
+  itemId: string;
+  userId: string;
+  upvotes: number;
+  hasUpvoted: boolean;
+  downvotes: number;
+  hasDownvoted: boolean;
+  hasSaved?: boolean;
+}
+
+const Votes = ({
+  type,
+  itemId,
+  userId,
+  upvotes,
+  hasUpvoted,
+  downvotes,
+  hasDownvoted,
+  hasSaved,
+}: Props) => {
+  // const handleSave = () = > {
+
+  // }
+
+  const handleVote = (action: string) => {};
+
+  return (
+    <div className="flex gap-5">
+      <div className="flex-center gap-2.5">
+        <div className="flex-center gap-1.5">
+          <Image
+            src={
+              hasUpvoted
+                ? "/assets/icons/upvoted.svg"
+                : "/assets/icons/upvote.svg"
+            }
+            width={18}
+            height={18}
+            alt="upvote"
+            className="cursor-pointer"
+            onClick={() => {
+              handleVote("upvote");
+            }}
+          />
+
+          <div className="flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1">
+            <p className="subtle-medium text-dark400_light900">
+              {formatNumber(upvotes)}
+            </p>
+          </div>
+        </div>
+
+        <div className="flex-center gap-1.5">
+          <Image
+            src={
+              hasDownvoted
+                ? "/assets/icons/downvoted.svg"
+                : "/assets/icons/downvote.svg"
+            }
+            width={18}
+            height={18}
+            alt="downvote"
+            className="cursor-pointer"
+            onClick={() => {
+              handleVote("downvote");
+            }}
+          />
+
+          <div className="flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1">
+            <p className="subtle-medium text-dark400_light900">
+              {formatNumber(downvotes)}
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <Image
+        src={
+          hasSaved
+            ? "/assets/icons/star-filled.svg"
+            : "/assets/icons/star-red.svg"
+        }
+        width={18}
+        height={18}
+        alt="star"
+        className="cursor-pointer"
+        onClick={() => {}}
+      />
+    </div>
+  );
 };
 
 export default Votes;
