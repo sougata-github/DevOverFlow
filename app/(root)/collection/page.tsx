4ec819e19161f8879d193d7833b8a5ff4fc0434b
Sougata <sougatadas9874@gmail.com>
2023-11-17 20:59:58 +0530

    fix: fix upvotes display issue.

--- app/(root)/collection/page.tsx
+++ app/(root)/collection/page.tsx
@@ -45,7 +45,7 @@ export default async function Home() {
               title={question.title}
               tags={question.tags}
               author={question.author}
-              upvotes={question.upvotes.length}
+              upvotes={question.upvotes}
               views={question.views}
               answers={question.answers}
               createdAt={question.createdAt}
