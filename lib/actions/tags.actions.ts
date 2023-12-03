d0084e4dffd66571f9d1903c6c72c7d9ac767ea1
Sougata <sougatadas9874@gmail.com>
2023-12-03 10:38:45 +0530

    fix: remove unknown property from getQuestionsByIdTagId server action.

--- lib/actions/tags.actions.ts
+++ lib/actions/tags.actions.ts
@@ -51,7 +51,7 @@ export async function getQuestionsByTagId(params: GetQuestionsByTagIdParams) {
   try {
     connectToDatabase();
 
-    const { tagId, page = 1, pageSize = 10, filter, searchQuery } = params;
+    const { tagId, page = 1, pageSize = 10, searchQuery } = params;
 
     const tagFilter: FilterQuery<ITag> = { _id: tagId };
 
